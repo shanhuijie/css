792557f1220dbb6c56dcc5dd45d23d6429222d37
shanhuijie <2270149665@qq.com>
2019-11-12 15:50:27 +0800

    '2019/11/12-login-finish'

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 new Vue({
-    el: '#app',
-    data: function() {
-      return { visible: false }
+  el: '.app',
+  data() {
+    const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(10).fill(item)
+      }
+  },
+  
+  methods:{
+    singleUpload(){
+        
     }
-  })
 
+  }
+
+})
